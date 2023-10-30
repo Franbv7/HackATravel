85607712ce8a23c5cb819b8a27ea64eb6c77115d
Fran <fran.bana.vazquez@gmail.com>
2023-10-30 21:07:22 +0100

    puerto

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,14 @@
 const server = require("./src/app");
 
+const app = express();
+
 const PORT = process.env.PORT || 3001;
 
 process.on("uncaughtException", function (err) {
   console.log(err);
 });
+
+app.listen(PORT);
 
 server.listen(PORT, () => {
   console.log(`Servidor escuchando en puerto ${PORT}`);
